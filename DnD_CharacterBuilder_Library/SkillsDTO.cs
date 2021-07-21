d54e8417719d7d537dbfc1fee8d30f71298183de
Rónai Péter <asdafa45@gmail.com>
2021-07-21 15:44:01 +0200

    Updates

--- DnD_CharacterBuilder_Library/SkillsDTO.cs
+++ DnD_CharacterBuilder_Library/SkillsDTO.cs
@@ -6,23 +6,23 @@ namespace DnD_CharacterBuilder_Library
 {
     class SkillsDTO
     {
-        public static bool Acrobatics { get; set; }
-        public static bool AnimalHandling { get; set; }
-        public static bool Arcana { get; set; }
-        public static bool Athletics { get; set; }
-        public static bool Deception { get; set; }
-        public static bool History { get; set; }
-        public static bool Insight { get; set; }
-        public static bool Intimidation { get; set; }
-        public static bool Investigation { get; set; }
-        public static bool Medicine   { get; set; }
-        public static bool Nature { get; set; }
-        public static bool Perception { get; set; }
-        public static bool Performance { get; set; }
-        public static bool Persuasion { get; set; }
-        public static bool Religion { get; set; }
-        public static bool SleightofHand { get; set; }
-        public static bool Stealth { get; set; }
-        public static bool Survival { get; set; }
+        public static bool Acrobatics { get; set; } = false;
+        public static bool AnimalHandling { get; set; } = false;
+        public static bool Arcana { get; set; } = false;
+        public static bool Athletics { get; set; } = false;
+        public static bool Deception { get; set; } = false;
+        public static bool History { get; set; } = false;
+        public static bool Insight { get; set; } = false;
+        public static bool Intimidation { get; set; } = false;
+        public static bool Investigation { get; set; } = false;
+        public static bool Medicine   { get; set; } = false;
+        public static bool Nature { get; set; } = false;
+        public static bool Perception { get; set; } = false;
+        public static bool Performance { get; set; } = false;
+        public static bool Persuasion { get; set; } = false;
+        public static bool Religion { get; set; } = false;
+        public static bool SleightofHand { get; set; } = false;
+        public static bool Stealth { get; set; } = false;
+        public static bool Survival { get; set; } = false;
     }
 }
